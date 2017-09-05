fae4cbdc046f4b768118d3bd5081c6e9c45096dc
chengcheng <cheng.cheng@haoqitech.com.cn>
2017-09-05 18:41:18 +0800

    提交

---
+++ day5/demo/03.js
@@ -0,0 +1,49 @@
+/* 03 小小留言本 */
+
+var express = require('express');
+var app = express();
+var formidable = require('formidable');
+var db = require('./model/dbs.js')
+
+app.use(express.static('./public'))
+
+
+app.post('/addMess',function(req,res){
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+        console.log(fields);
+        var ajaxdata = {
+            'name':fields.name,
+            'mess':fields.mess,
+            'time':new Date()
+        }
+        db.insertOne('ceshi',ajaxdata,function(err,result){
+            if(err){
+                res.json({retcode:0,info:['失败']})
+                return
+            }
+            res.json({retcode:1,data:ajaxdata,info:['提交成功']})
+        })
+    });
+
+})
+
+app.post('/allMess',function(req,res){
+    var form = new formidable.IncomingForm();
+
+    db.find('ceshi',{},function(err,result){
+        res.json({retcode:1,data:result,info:['提交成功']})
+    })
+
+})
+
+
+
+
+
+
+
+
+
+app.listen(8080)
--- day5/demo/model/collection.js
+++ day5/demo/model/collection.js
@@ -1,3 +1,3 @@
 module.exports = {
-    dblion:"mongodb://localhost:27017/lion"
+    dburl:"mongodb://localhost:27017/lion"
 }
