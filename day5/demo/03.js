f951a99e1d569f1ea28de5c162086584213acf94
chengcheng <cheng.cheng@haoqitech.com.cn>
2017-09-06 12:23:48 +0800

    留言本增删改查

--- day5/demo/03.js
+++ day5/demo/03.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var formidable = require('formidable');
 var db = require('./model/dbs.js')
+var ObjectId = require('mongodb').ObjectID;
 
 app.use(express.static('./public'))
 
@@ -12,7 +13,6 @@ app.post('/addMess',function(req,res){
     var form = new formidable.IncomingForm();
 
     form.parse(req, function(err, fields, files) {
-        console.log(fields);
         var ajaxdata = {
             'name':fields.name,
             'mess':fields.mess,
@@ -34,6 +34,25 @@ app.post('/allMess',function(req,res){
 
     db.find('ceshi',{},function(err,result){
         res.json({retcode:1,data:result,info:['提交成功']})
+    })
+
+})
+app.get('/du',function(req,res){
+    var page = req.query.page;
+    var form = new formidable.IncomingForm();
+    db.find('ceshi',{},{"sort":{"shijian":-1},"pageamount":5,"page":page},function(err,result){
+        db.getAllCount('ceshi',function(count){
+            var pageNum = parseInt(count/5)+1
+            res.json({retcode:1,data:result,pageNum:pageNum,info:['提交成功']})
+        })
+    })
+
+})
+app.get('/shanchu',function(req,res){
+    var id = req.query.id
+    var form = new formidable.IncomingForm();
+    db.deleteMany('ceshi',{_id:ObjectId(id)},function(err,result){
+        res.json({retcode:1,data:result,info:['删除成功']})
     })
 
 })
