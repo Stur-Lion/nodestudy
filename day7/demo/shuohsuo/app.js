d7e1442f3802b33d5e8d7a166996736f386c63a6
chengcheng <cheng.cheng@haoqitech.com.cn>
2017-09-19 17:40:54 +0800

    登录

--- day7/demo/shuohsuo/app.js
+++ day7/demo/shuohsuo/app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 var shuo = require('./router/router.js')
+var session = require('express-session')
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
 
 //模板引擎级静态资源
 app.use(express.static('./public'))
@@ -8,6 +15,8 @@ app.set('view engine','ejs')
 
 
 app.get('/',shuo.showIndex)//进入首页
+app.post('/doIndex',shuo.doIndex)//登录
+app.post('/outlogin',shuo.outLogin)//退出登录
 app.get('/regist',shuo.showRegit)//注册页面
 app.post('/doregist',shuo.showRegited)//注册
 
--- day7/demo/shuohsuo/router/router.js
+++ day7/demo/shuohsuo/router/router.js
@@ -5,9 +5,44 @@ var path = require("path");
 var fs = require("fs");
 var gm = require("gm");
 
-//首页登录
+//首页登录页面
 exports.showIndex=function(req,res){
-    res.render('login',{})
+    res.render('login',{
+        login:req.session.login,
+        usernmae:req.session.name
+    })
+}
+
+//登录
+exports.doIndex = function(req,res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        console.log(fields);
+        db.find('user',{
+            name:fields.name,
+            password:fields.password},
+            function(err,result){
+                if(err){
+                    res.json({data:{},info:['登录失败'],retcode:0})
+                }
+                console.log(result);
+                if(result.length==1){
+                    req.session.login=1;
+                    req.session.name=fields.name;
+                    res.json({data:{},info:['登录成功'],retcode:1})
+                }else{
+                    res.json({data:{},info:['登录失败'],retcode:0})
+                }
+            }
+        )
+    });
+}
+
+//退出登录
+exports.outLogin=function(req,res){
+    req.session.login=0;
+    req.session.name='';
+    res.json({data:{},info:['退出成功'],retcode:1})
 }
 
 //注册页面
@@ -21,6 +56,8 @@ exports.showRegited = function(req,res){
     form.parse(req, function(err, fields, files) {
         console.log(fields);
         db.insertOne('user',fields,function(){
+            req.session.login=1;
+            req.session.name=fields.name;
             res.json({data:{},info:['添加成功'],retcode:1})
         })
     });
