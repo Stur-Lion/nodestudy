21cc0bc5834d96949c18f2e61fd977ba38d288d6
chengcheng <chengcheng@haoqitech.com.cn>
2017-11-01 18:04:39 +0800

    默认的

--- day8/demo/02.js
+++ day8/demo/02.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+console.log(mongoose);
 
 mongoose.connect('mongodb://localhost/demo', { useMongoClient: true });
 
@@ -8,33 +9,42 @@ var mySchema = new mongoose.Schema({
     age:Number,
     email:String
 })
-var a,b;
+var aa,bb;
+/*var a,b;
 mySchema.statics.showInfo = function(callback){
     console.log(this);
     a=this
     this.findOne({username:'lion'},callback)
-}
-mySchema.methods.showInfo = function(callback){
-    console.log(this);
+}*/
+mySchema.statics.showInfo = function(callback){
+    /*console.log(this);
     this.model('demo').findOne({username: 'lion'},callback)
     console.log(this.model('demo'));
-    b = this.model('demo')
+    b = this.model('demo')*/
+    //console.log(this);
+    /*this.find({},callback)*/
+    //aa=this.model('demo')
+    this.find({},{username:true},callback)
 }
 
 var myModel = mongoose.model('demo',mySchema)
 
 var blog = new myModel({
     username:'lion',
-    age:20,
-    email:'150@qq.com'
+    age:30,
+    email:'150@qq.com',
 })
 /*blog.save()*/
+/*blog.showInfo(function(err,user){
+    console.log(user);
+    console.log(myModel == aa);
+})*/
 myModel.showInfo(function(err,users){
     console.log(users);
 })
-blog.showInfo(function(err,user){
+ /*blog.showInfo(function(err,user){
     console.log(user);
     console.log(a == b);
     console.log(a == myModel);
-})
+})*/
 
