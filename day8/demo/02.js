86119c59934106fe7516df9392331adb01249ccb
chengcheng <chengcheng@haoqitech.com.cn>
2017-11-01 15:35:14 +0800

    提交代码

---
+++ day8/demo/02.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/demo', { useMongoClient: true });
+console.log(mongoose);
+
+
+var mySchema = new mongoose.Schema({
+    username:String,
+    age:Number,
+    email:String
+})
+var a,b;
+mySchema.statics.showInfo = function(callback){
+    //console.log(this);
+    a=this
+    this.findOne({username:'lion'},callback)
+}
+mySchema.methods.showInfo = function(callback){
+    //console.log(this);
+    this.model('demo').findOne({username: 'lion'},callback)
+    //console.log(this.model('demo'));
+    b = this.model('demo')
+}
+
+var myModel = mongoose.model('demo',mySchema)
+
+var blog = new myModel({
+    username:'lion',
+    age:20,
+    email:'150@qq.com'
+})
+/*blog.save()*/
+myModel.showInfo(function(err,users){
+    //console.log(users);
+})
+blog.showInfo(function(err,user){
+    /*console.log(user);
+    console.log(a == b);
+    console.log(a == myModel);
+    console.log(mongoose);*/
+})
+
